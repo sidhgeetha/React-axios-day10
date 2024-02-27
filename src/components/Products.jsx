3b0953abae7d774b4e32575ad9fb334aefef8a1a
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-02-27 21:41:16 +0000

    changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -44,65 +44,69 @@ const Products = ({ setId }) => {
 
   return (
     <div style={{ margin: "60px" }}>
-
-      <div class=" container d-grid gap-2 d-md-flex justify-content-md-end" style={{margin:"16px"}}>
-        <button style={{width:"100px"}}
+      <div
+        class=" container d-grid gap-2 d-md-flex justify-content-md-end"
+        style={{ margin: "16px" }}
+      >
+        <button
+          style={{ width: "100px" }}
           class="btn btn-primary"
           onClick={() => {
             navigate("/ProductCreate");
-          }} >
+          }}
+        >
           Create
         </button>
       </div>
+      <div class="table-responsive">
+        <table class="table" style={{ border: "1px solid #ccc" }}>
+          <thead>
+            <tr>
+              <th scope="col">Num</th>
+              <th scope="col">FoodId</th>
+              <th scope="col">FoodName</th>
+              <th scope="col">FoodPrice</th>
+              <th scope="col">FoodDescription</th>
+              <th scope="col"> Edit </th>
+              <th scope="col"> Delete </th>
+            </tr>
+          </thead>
 
-      <table class="table" style={{ border: "1px solid #ccc" }}>
-        <thead>
-          <tr>
-            <th scope="col">Num</th>
-            <th scope="col">FoodId</th>
-            <th scope="col">FoodName</th>
-            <th scope="col">FoodPrice</th>
-            <th scope="col">FoodDescription</th>
-            <th scope="col"> Edit </th>
-            <th scope="col"> Delete </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {products.map((item, index) => {
-            return (
-              <>
-                <tr key={index}>
-                  <th scope="row">{item.id}</th>
-                  <td>{item.food_id}</td>
-                  <td>{item.food_name}</td>
-                  <td>{item.food_price}</td>
-                  <td>{item.food_description}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        handleEdit(item.id);
-                      }}
-                    >
-                      Edit
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        handleDelete(item.id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-        </tbody>
-      
-      </table>
+          <tbody>
+            {products.map((item, index) => {
+              return (
+                <>
+                  <tr key={index}>
+                    <th scope="row">{item.id}</th>
+                    <td>{item.food_id}</td>
+                    <td>{item.food_name}</td>
+                    <td>{item.food_price}</td>
+                    <td>{item.food_description}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          handleEdit(item.id);
+                        }}
+                      >
+                        Edit
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          handleDelete(item.id);
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
